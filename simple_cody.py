1bf27b445aaf5876676f74cddf1ae93c67900246
Osho8k <oshoasegh@gmail.com>
2025-11-10 19:54:50 +0100

    simple_cody.py

---
+++ simple_cody.py
@@ -0,0 +1,20 @@
+#Team Tyrosine Information Collector
+print("Team Tyrosine")
+
+name = input("Enter your full name: ")
+slack_username = input("Enter your slack username: ")
+country = input("Enter your country: ")
+hobby = input("Enter your hobby: ")
+affiliations = input("Enter your affiliations: ")
+gene = input("Enter your favorite gene here: ")
+dna = input("Enter the DNA sequence: ")
+
+print("\n Team Member Info ")
+print(f"Name: {name}")
+print(f"Slack: {slack_username}")
+print(f"Country: {country}")
+print(f"Hobby: {hobby}")
+print(f"Affiliations: {affiliations}")
+print(f"Favorite Gene: {gene}")
+print(f"DNA Sequence: {dna}")
+     
